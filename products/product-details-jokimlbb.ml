fcb3f545c61584cf3dba87a884e33d30014691c5
kelzhost <153807466+Aryaazka@users.noreply.github.com>
2024-01-15 13:21:41 +0000

    Add files via upload

---
+++ products/product-details-jokimlbb.ml
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboard-style.css">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <div class="dashboard-container">
+        <!-- Produk 1: VPS -->
+        <div class="product-box">
+            <h3>JOKI MLBB PERBBINTANG</h3>
+            <p>GRANDMASTER.</p>
+            <p class="price">Rp5.000/PER/BINTANG</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'VPS Product 2', 20.000)">Buy Now</button>
+            </div>
+        </div>
+
+<div class="product-box">
+            <h3>JOKI MLBB PERBBINTANG</h3>
+            <p>EPIC.</p>
+            <p class="price">Rp6.000/PER/BINTANG</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'VPS Product 2', 20.000)">Buy Now</button>
+            </div>
+        </div>
+        
+<div class="product-box">
+            <h3>JOKI MLBB PERBBINTANG</h3>
+            <p>EPIC-LEGEND.</p>
+            <p class="price">Rp20.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'VPS Product 2', 20.000)">Buy Now</button>
+            </div>
+        </div>
+
+<div class="product-box">
+            <h3>JOKI MLBB PERBBINTANG</h3>
+            <p>LEGEND-MYTHIC.</p>
+            <p class="price">Rp20.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'VPS Product 2', 20.000)">Buy Now</button>
+            </div>
+        </div>
+
+        <!-- Produk 2: RDP -->
+<div class="product-box">
+            <h3>JOKI MLBB PERBBINTANG</h3>
+            <p>MYTHIC-MTHIC HONOR.</p>
+            <p class="price">Rp20.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'VPS Product 2', 20.000)">Buy Now</button>
+            </div>
+        </div>
+
+<div class="product-box">
+            <h3>JOKI MLBB PERBBINTANG</h3>
+            <p>HONOR-GLORY.</p>
+            <p class="price">Rp20.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'VPS Product 2', 20.000)">Buy Now</button>
+            </div>
+        </div>
+
+<div class="product-box">
+            <h3>JASA MABAR MLBB<</h3>
+            <p>RDP yang kami gunakan RDP Azure kami membuat RDP saat ada yang order RDP fresh GOOD QUALITY.</p>
+            <p class="price">Rp35.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'RDP Product 3', 35.000)">Buy Now</button>
+            </div>
+        </div>
+
+<div class="product-box">
+            <h3>RDP Windows 2022 8Gib Core2</h3>
+            <p>RDP yang kami gunakan RDP Azure kami membuat RDP saat ada yang order RDP fresh GOOD QUALITY.</p>
+            <p class="price">Rp45.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'RDP Product 4', 45.000)">Buy Now</button>
+            </div>
+        </div>
+
+        <!-- Produk 3: Domain -->
+        <div class="product-box">
+            <h3>Domain .my.id </h3>
+            <p>FREE Akses Cloud Flare 100%.</p>
+            <p class="price">Rp10.000/year</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Domain Product 1', 10.000)">Buy Now</button>
+            </div>
+        </div>
+
+ <div class="product-box">
+            <h3>Domain .biz.id </h3>
+            <p>FREE Akses Cloud Flare 100%.</p>
+            <p class="price">Rp10.000/year</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Domain Product 2', 10.000)">Buy Now</button>
+            </div>
+        </div>
+
+ <div class="product-box">
+            <h3>Domain .bid </h3>
+            <p>FREE Akses Cloud Flare 100%.</p>
+            <p class="price">Rp10.000/year</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Domain Product 3', 25.000)">Buy Now</button>
+            </div>
+        </div>
+        
+ <div class="product-box">
+            <h3>Domain .date </h3>
+            <p>FREE Akses Cloud Flare 100%.</p>
+            <p class="price">Rp25.000/year</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Domain Product 4', 25.000)">Buy Now</button>
+            </div>
+        </div>
+
+        <!-- Produk 4: Pterodactyl -->
+        <div class="product-box">
+            <h3>Panel Bot wa </h3>
+            <h3>Ram4  4Gib Cpu110</h3>
+            <p>Panel ini bisa di buat run 24jam tanpa hrs membuang banyak kuota jika sudah di run tutup broser maka bot wa akan tetap berjalan.</p>
+            <p class="price">Rp5.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Pterodactyl Product 1', 5.000)">Buy Now</button>
+            </div>
+        </div>
+        
+        <div class="product-box">
+            <h3>Panel Bot wa </h3>
+            <h3>Ram6 6Gib Cpu170</h3>
+            <p>Panel ini bisa di buat run 24jam tanpa hrs membuang banyak kuota jika sudah di run tutup broser maka bot wa akan tetap berjalan.</p>
+            <p class="price">Rp9.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Pterodactyl Product 2', 9.000)">Buy Now</button>
+            </div>
+        </div>
+
+        <div class="product-box">
+            <h3>Panel Bot wa </h3>
+            <h3>Ram9  9Gib Cpu190</h3>
+            <p>Panel ini bisa di buat run 24jam tanpa hrs membuang banyak kuota jika sudah di run tutup broser maka bot wa akan tetap berjalan.</p>
+            <p class="price">Rp12.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Pterodactyl Product 3', 12.000)">Buy Now</button>
+            </div>
+        </div>
+        
+        <div class="product-box">
+            <h3>Panel Bot wa </h3>
+            <h3>Unli  0Gib Cpu0</h3>
+            <p>Panel ini bisa di buat run 24jam tanpa hrs membuang banyak kuota jika sudah di run tutup broser maka bot wa akan tetap berjalan.</p>
+            <p class="price">Rp15.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Pterodactyl Product 4', 15.000)">Buy Now</button>
+            </div>
+        </div>
+
+
+        <!-- Produk 5: Script -->
+        <div class="product-box">
+            <h3>Script Bot Wa</h3>
+            <p>Kami menjual berbagai Script.</p>
+            <p class="price">15.000</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Script Product 1', 15.000)">Buy Now</button>
+            </div>
+        </div>
+        
+      <div class="product-box">
+            <h3>Script Website Jualan</h3>
+            <p>Kami menjual berbagai Script.</p>
+            <p class="price">25.000</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Script Product 2', 25.000)">Buy Now</button>
+            </div>
+        </div>
+
+        <!-- Produk 6: Reseller Domain -->
+        <div class="product-box">
+            <h3>Reseller SubDomain  </h3>
+            <p>Jika ingin join reseller Subdomain.Harap Buy.</p>
+            <p class="price">5.000/month</p>
+            <div class="buy-now-box">
+                <button class="buy-now-button" onclick="openPopup('https://telegra.ph/file/c57ecc7daabe0f6e11e9c.jpg', 'Reseller Domain Product 1', 5.000)">Buy Now</button>
+            </div>
+        </div>
+
+        <!-- ... (Tambahkan produk lainnya) ... -->
+
+    </div>
+
+    <!-- Popup -->
+    <div id="popup" class="popup-container">
+        <div class="popup-content">
+            <span class="close-button" onclick="closePopup()">&times;</span>
+            <h2 id="popup-product-name">Product Name</h2>
+            <p id="popup-product-description">Description goes here.</p>
+            <p id="popup-product-price">Price: $0.00</p>
+            <img id="qris-image" src="" alt="QR Code for Payment">
+        </div>
+    </div>
+
+    <!-- Welcome Section -->
+    <div class="welcome-container">
+        <h1 id="welcome-text">Selamat Satang di halaman KelzHost</h1>
+    </div>
+
+    <script src="dashboard-script.js"></script>
+</body>
+
+</html>
